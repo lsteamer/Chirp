b18b872c8814af56c7b00202a9abdfe981c2e172
Antonio Gonzalez <lsteamer@gmail.com>
2025-11-07 14:49:19 +0100

    Util - Created String Resource utility

--- core/presentation/build.gradle.kts
+++ core/presentation/build.gradle.kts
@@ -15,6 +15,8 @@ kotlin {
                 implementation(libs.kotlin.stdlib)
                 // Add KMP dependencies here
                 implementation(projects.core.domain)
+
+                implementation(compose.components.resources)
             }
         }
 
