54c65a647725ef38d311743b6bc12f9d1a5c76b6
Antonio Gonzalez <lsteamer@gmail.com>
2025-11-17 21:49:40 +0100

    Compose - Reworked ChirpTextField

--- core/designsystem/src/commonMain/kotlin/com/anjegonz/core/designsystem/components/textfields/ChirpTextField.kt
+++ core/designsystem/src/commonMain/kotlin/com/anjegonz/core/designsystem/components/textfields/ChirpTextField.kt
@@ -1,17 +1,8 @@
 package com.anjegonz.core.designsystem.components.textfields
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.interaction.collectIsFocusedAsState
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.text.input.TextFieldLineLimits
@@ -20,9 +11,6 @@ import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.SolidColor
@@ -46,96 +34,51 @@ fun ChirpTextField(
     onFocusChanged: (Boolean) -> Unit = {},
 ) {
 
-    val interactionSource = remember {
-        MutableInteractionSource()
-    }
-    val isFocused by interactionSource.collectIsFocusedAsState()
-
-    LaunchedEffect(isFocused) {
-        onFocusChanged(isFocused)
-    }
-    Column(modifier = modifier) {
-        if (title != null) {
-            Text(
-                text = title,
-                style = MaterialTheme.typography.labelSmall,
-                color = MaterialTheme.colorScheme.extended.textSecondary
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-
-            BasicTextField(
-                state = state,
-                enabled = enabled,
-                lineLimits = if (singleLine) {
-                    TextFieldLineLimits.SingleLine
-                } else TextFieldLineLimits.Default,
-                textStyle = MaterialTheme.typography.bodyMedium.copy(
-                    color = if (enabled) {
-                        MaterialTheme.colorScheme.surface
-                    } else {
-                        MaterialTheme.colorScheme.extended.textPlaceholder
-                    },
-                ),
-                keyboardOptions = KeyboardOptions(
-                    keyboardType = keyboardType
-                ),
-                cursorBrush = SolidColor(MaterialTheme.colorScheme.onSurface),
-                interactionSource = interactionSource,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .background(
-                        color = when {
-                            isFocused -> MaterialTheme.colorScheme.primary.copy(
-                                alpha = 0.05f
-                            )
-
-                            enabled -> MaterialTheme.colorScheme.surface
-                            else -> MaterialTheme.colorScheme.extended.secondaryFill
-                        },
-                        shape = RoundedCornerShape(8.dp)
-                    )
-                    .border(
-                        width = 1.dp,
-                        color = when {
-                            isError -> MaterialTheme.colorScheme.error
-                            isFocused -> MaterialTheme.colorScheme.primary
-                            else -> MaterialTheme.colorScheme.outline
-                        },
-                        shape = RoundedCornerShape(8.dp)
-                    )
-                    .padding(12.dp),
-                decorator = { innerBox ->
-                    Box(
-                        modifier = Modifier
-                            .fillMaxWidth(),
-                        contentAlignment = Alignment.CenterStart
-                    ) {
-                        if (state.text.isEmpty() && placeholder != null) {
-                            Text(
-                                text = placeholder,
-                                color = MaterialTheme.colorScheme.extended.textPlaceholder,
-                                style = MaterialTheme.typography.bodyMedium
-                            )
-                        }
-                        innerBox()
+    ChirpTextFieldLayout(
+        title = title,
+        isError = isError,
+        supportingText = supportingText,
+        enabled = enabled,
+        onFocusChanged = onFocusChanged,
+        modifier = modifier,
+    ) { styleModifier, interactionSource ->
+        BasicTextField(
+            state = state,
+            enabled = enabled,
+            lineLimits = if (singleLine) {
+                TextFieldLineLimits.SingleLine
+            } else TextFieldLineLimits.Default,
+            textStyle = MaterialTheme.typography.bodyMedium.copy(
+                color = if (enabled) {
+                    MaterialTheme.colorScheme.surface
+                } else {
+                    MaterialTheme.colorScheme.extended.textPlaceholder
+                },
+            ),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = keyboardType
+            ),
+            cursorBrush = SolidColor(MaterialTheme.colorScheme.onSurface),
+            interactionSource = interactionSource,
+            modifier = styleModifier,
+            decorator = { innerBox ->
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    contentAlignment = Alignment.CenterStart
+                ) {
+                    if (state.text.isEmpty() && placeholder != null) {
+                        Text(
+                            text = placeholder,
+                            color = MaterialTheme.colorScheme.extended.textPlaceholder,
+                            style = MaterialTheme.typography.bodyMedium
+                        )
                     }
+                    innerBox()
                 }
-            )
-            if(supportingText != null) {
-                Spacer(modifier = Modifier.height(4.dp))
-                Text(
-                    text = supportingText,
-                    color = if(isError) {
-                        MaterialTheme.colorScheme.error
-                    } else {
-                        MaterialTheme.colorScheme.extended.textTertiary
-                    },
-                    style = MaterialTheme.typography.bodySmall
-                )
             }
-        }
+        )
     }
-
 }
 
 @Composable
